d3ba87745ce2ebac7e02e49adde66e731578a12e
Yusuf-Li7 <mira2323@mail.ru>
2021-02-08 15:45:53 +0300

    Update ex3.c

--- lab_3/ex3.c
+++ lab_3/ex3.c
@@ -4,7 +4,7 @@
 
 int main(int argc, char* argv[], char *envp[]) {
 	
-  printf("Information about argument line:\n");
+  	printf("Information about argument line:\n");
 	for (int i = 0; i < argc; i++) {
 		printf("%s\n", argv[i]);
 	}
