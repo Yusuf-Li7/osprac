1ab448a9a4ed74f950bc47973969e8429aefc2be
Yusuf-Li7 <mira2323@mail.ru>
2021-02-12 12:11:46 +0300

    Update ex4.c

--- lab_3/ex4.c
+++ lab_3/ex4.c
@@ -1,18 +1,16 @@
 #include <stdio.h>
-#include <stdlib.h>
+#include <sys/types.h>
 #include <unistd.h>
-
-int main(int argc, char* argv[], char* envp[])
-{
-  pid_t pid = fork();
+int main(int argc, char *argv[], char *envp[]) {
+    pid_t pid = fork();
     if(pid == -1){
         printf("Error");
     } else if (pid == 0){
         printf("Child pid = %d\n", (int)getpid());
-        (void)execle("/bin/cat", "/bin/cat", "Hello,", "world!", 0, envp);
+        (void)execle("/bin/echo", "/bin/echo", "Hello,", "world!", 0, envp);
     } else {
         printf("Parent pid = %d\n", (int)getpid());
     }
- 
-  return 0;
+    
+    return 0;
 }
