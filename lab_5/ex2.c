e2249995f68be9b4daf0c5b70a80082b134f3182
Yusuf-Li7 <mira2323@mail.ru>
2021-02-15 23:35:24 +0300

    Update ex2.c

--- lab_5/ex2.c
+++ lab_5/ex2.c
@@ -1,30 +1,44 @@
 #include <sys/types.h>
-#include <stdio.h>
 #include <unistd.h>
+#include <stdio.h>
 #include <stdlib.h>
 
 int main()
 {
-int fd[2];
-ssize_t size;
-char string[] = "Hello, world!";
-char resstring[14];
+  int     fd[2];
+  ssize_t size;
+  char     string[] = "Hello, world!";
+  char  resstring[14];
 
-if (pipe(fd) < 0) {
-printf("Pipe didnt open\n");
-exit(-1);
-}
+  if (pipe(fd) < 0) {
+    printf("Pipe didnt open\n");
+    exit(-1);
+  }
 
-size = write(fd[1], string, 14);
+  size = write(fd[1], string, 14);
 
-if (size != 14) {
-printf("Pipe didnt write all string\n");
-exit(-1);
-}
+  if (size != 14) {
+    printf("Pipe didnt write all string\n");
+    exit(-1);
+  }
 
-size = read(fd[0], resstring, 14);
+  size = read(fd[0], resstring, 14);
 
-if (size < 0) {
-printf("Pipe didnt read string from\n");
-exit(-1);
+  if (size < 0) {
+    printf("Pipe didnt read string\n");
+    exit(-1);
+  }
+
+  printf("%s\n", resstring);
+
+  if (close(fd[0]) < 0) {
+    printf("Pipe didnt close reading side\n"); 
+    exit(-1);
+  }
+  if (close(fd[1]) < 0) {
+    printf("Pipe didnt close writing side\n"); 
+    exit(-1);
+  }
+
+  return 0;
 }
