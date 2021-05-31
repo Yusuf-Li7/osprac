ec43fe1fb39ded81a62d53758a293e05ac3df6e2
Yusuf-Li7 <mira2323@mail.ru>
2021-05-31 15:49:30 +0300

    Create ex1.c

---
+++ ex1.c
@@ -0,0 +1,13 @@
+#include <signal.h>
+
+int main() {
+  
+	// We ignore SIGINT and SIGQUIT
+	signal(SIGINT, SIG_IGN);
+	signal(SIGQUIT, SIG_IGN);
+  
+	// Inifinite loop, wait until termination
+	while (1);
+  
+	return 0;
+}
